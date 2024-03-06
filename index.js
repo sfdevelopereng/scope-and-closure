12c4e5cea916e01de43ce15c218f54c8d8ce761b
sfdevelopereng <thugz-roger@hotmail.com>
2024-03-06 12:05:09 -0800

    solved few challenges

--- index.js
+++ index.js
@@ -64,9 +64,12 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/g
-}
+function inning(){
+  let points = Math.ceil(Math.random() * 2)
+  
+    return points
+  } 
+
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -83,8 +86,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(track, innings){
+  const score = {Home: 0,
+  Away: 0}
+  for(let i = 0; i < innning; i++){
+    points = track();
+    score.Home += points
+  }
+  return score
 }
 
 
@@ -101,8 +110,10 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(getPoints) {
+  score.Home = getPoints();
+  score.Away = getPoints();
+  
 
 }
 
